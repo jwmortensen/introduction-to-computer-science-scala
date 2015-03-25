0caa31d1ad4f861a9d1a7a93460e5da32ed04d44
Jacob Mortensen <jacob.w.mortensen@gmail.com>
2015-03-25 09:42:21 -0600

    Attempt to create more functional menu

--- plinko/Plinko.scala
+++ plinko/Plinko.scala
@@ -1,28 +1,35 @@
 import scala.io.StdIn
 
 object Plinko {
-  def main(args: Array[String]) {
-    var quit = 0
-    while (quit == 0) {
-      var game = -1
-      println("""|Please select one of the following:
-    |1 - drop one token 
-    |2 - drop multiple tokens
-    |3 - quit""".stripMargin)
-      game = StdIn.readInt
-      if (game == 1) {
-        println("selected 1")
-      }
-      else if (game == 2) {
-        println("selected 2")
-      }
-      else if (game == 3) {
-        println("selected 3")
-        quit = 1
-      }
-      else {
-        println("Sorry, that command is not recognized.")
-      }
+  def menu(option: Int) {
+    println("""|Please select one of the following:
+                 |  1 - drop one token 
+                 |  2 - drop multiple tokens
+                 |  3 - quit""".stripMargin)
+    if (option == 1) {
+      println("selected 1")
+      val opt = StdIn.readInt
+      menu(opt)
     }
+    else if (option == 2) {
+      println("selected 2")
+      val opt = StdIn.readInt
+      menu(opt)
+    }
+    else if (option == 3) {
+      println("selected 3")
+    }
+    else {
+      println("Sorry, that command is not recognized")
+    }
+  }
+
+  def main(args: Array[String]) {
+    println("""|Please select one of the following:
+                 |  1 - drop one token 
+                 |  2 - drop multiple tokens
+                 |  3 - quit""".stripMargin)
+    val opt = StdIn.readInt
+    menu(opt)
   }
 }
