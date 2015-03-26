9cb37c86527d3022b0c039e01b4b5611a8d1826a
Jacob Mortensen <jacob.w.mortensen@gmail.com>
2015-03-26 00:43:29 -0600

    Started thinking about how to rewrite other parts of the code functionally.

--- plinko/Plinko.scala
+++ plinko/Plinko.scala
@@ -1,10 +1,28 @@
 import scala.io.StdIn
 
 object Plinko {
+  def slotPrize(slot: Int): Int = {
+    slot match {
+      case 0 | 8 => 100
+      case 1 | 7 => 500
+      case 2 | 6 => 1000
+      case 3 | 5 => 0
+      case 4 => 10000
+      case _ => 
+        println("An error occured. Please check the slotPrize function")
+        0
+    }
+  }
+
+  def dropOne = {
+    val slot = selectSlot
+    println(slot)
+  }    
+
   def menu(option: Int): Boolean = {
     option match {
       case 1 =>
-        println("selected 1")
+        dropOne
         true
       case 2 =>
         println("selected 2")
@@ -25,9 +43,20 @@ object Plinko {
                  |  3 - quit""".stripMargin)
     StdIn.readInt
   }
-    
+   
+  def selectSlot: Int = {
+    println("Please select a slot between 0 and 8")
+    var slot = StdIn.readInt
+    if (slot >= 0 & slot <= 8) {
+      slot
+    }
+    else {
+      println("Sorry, you must pick a valid slot between 0 and 8.")
+      selectSlot
+    }
+  }
 
-  def main(args: Array[String]) {
+  def main(args: Array[String]) = {
     var opt = -1
     do {
       opt = readOption
