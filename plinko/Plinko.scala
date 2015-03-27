9220d7d92ed91cbef41c08776c819ac25b70a5c3
Jacob Mortensen <jacob.w.mortensen@gmail.com>
2015-03-27 00:58:16 -0600

    Function to get token path

--- plinko/Plinko.scala
+++ plinko/Plinko.scala
@@ -1,6 +1,28 @@
 import scala.io.StdIn
+import scala.util.Random
 
 object Plinko {
+
+  def readOption: Int = {
+    println("""|Please select one of the following:
+                 |  1 - drop one token 
+                 |  2 - drop multiple tokens
+                 |  3 - quit""".stripMargin)
+    StdIn.readInt
+  }
+   
+  def selectSlot: Int = {
+    println("Please select a slot between 0 and 8")
+    var slot = StdIn.readInt
+    if (slot >= 0 & slot <= 8) {
+      slot
+    }
+    else {
+      println("Sorry, you must pick a valid slot between 0 and 8.")
+      selectSlot
+    }
+  }
+
   def slotPrize(slot: Int): Int = {
     slot match {
       case 0 | 8 => 100
@@ -14,8 +36,27 @@ object Plinko {
     }
   }
 
+  def getPath(prevSlot: Double): Stream[Double] = { 
+    var left = Random.nextBoolean
+    if (prevSlot <= 0.0) {
+      left = false
+    }
+    else if (prevSlot >= 8.0) {
+      left = true
+    }
+
+    if (left) {
+      prevSlot #:: getPath(prevSlot - 0.5) 
+    }
+    else {
+      prevSlot #:: getPath(prevSlot + 0.5)
+    }
+  }
+     
   def dropOne = {
     val slot = selectSlot
+    val path = getPath(slot).take(12)
+    println(path.toList.toString)
     println(slot)
   }    
 
@@ -36,25 +77,6 @@ object Plinko {
     }
   }
   
-  def readOption: Int = {
-    println("""|Please select one of the following:
-                 |  1 - drop one token 
-                 |  2 - drop multiple tokens
-                 |  3 - quit""".stripMargin)
-    StdIn.readInt
-  }
-   
-  def selectSlot: Int = {
-    println("Please select a slot between 0 and 8")
-    var slot = StdIn.readInt
-    if (slot >= 0 & slot <= 8) {
-      slot
-    }
-    else {
-      println("Sorry, you must pick a valid slot between 0 and 8.")
-      selectSlot
-    }
-  }
 
   def main(args: Array[String]) = {
     var opt = -1
