b3136cac60d2344b69ac94aebd718f1b822f4347
Jacob Mortensen <jacob.w.mortensen@gmail.com>
2015-03-28 20:59:08 -0600

    Finished plinko

--- plinko/Plinko.scala
+++ plinko/Plinko.scala
@@ -2,8 +2,9 @@ import scala.io.StdIn
 import scala.util.Random
 
 object Plinko {
+  val actionMap = Map[Int, () => Boolean](1 -> dropOne, 2 -> dropMany, 3 -> quit)
 
-  def readOption: Int = {
+  def readOption(): Int = {
     println("""|Please select one of the following:
                  |  1 - drop one token 
                  |  2 - drop multiple tokens
@@ -11,16 +12,24 @@ object Plinko {
     StdIn.readInt
   }
    
-  def selectSlot: Int = {
+  def selectSlot(): Int = {
     println("Please select a slot between 0 and 8")
-    var slot = StdIn.readInt
-    if (slot >= 0 & slot <= 8) {
-      slot
-    }
-    else {
-      println("Sorry, you must pick a valid slot between 0 and 8.")
-      selectSlot
-    }
+    val slot = StdIn.readInt
+    if (slot >= 0 & slot <= 8) slot else selectSlot
+  }
+
+  def numChips(): Int = {
+    println("Please enter the number of chips you would like to drop: ")
+    val num = StdIn.readInt
+    if (num > 0) num else numChips
+  }
+
+  def sum(xs: Stream[Int]): Double  = {
+    xs reduceLeft {(x,y) => x + y}
+  }
+
+  def avg(xs: Stream[Int]): Double  = {
+    sum(xs) / xs length
   }
 
   def slotPrize(slot: Double): Int = {
@@ -37,46 +46,48 @@ object Plinko {
   }
 
   def getPath(prevSlot: Double): Stream[Double] = { 
-    var left = Random.nextBoolean
-    if (prevSlot <= 0.0) {
-      left = false
-    }
-    else if (prevSlot >= 8.0) {
-      left = true
-    }
-
-    if (left) {
-      prevSlot #:: getPath(prevSlot - 0.5) 
-    }
-    else {
-      prevSlot #:: getPath(prevSlot + 0.5)
+    prevSlot match {
+      case 0 => prevSlot #:: getPath(prevSlot + 0.5)
+      case 8 => prevSlot #:: getPath(prevSlot - 0.5)
+      case _ => 
+        if (Random.nextBoolean) prevSlot #:: getPath(prevSlot - 0.5) else prevSlot #:: getPath(prevSlot + 0.5)
     }
   }
      
-  def dropOne = {
+  def getMultipleWinnings(slot: Double): Stream[Int] = {
+    slotPrize(getPath(slot).take(13).last) #:: getMultipleWinnings(slot)
+  }
+
+  def dropOne(): Boolean = {
     val slot = selectSlot
     val path = getPath(slot).take(13).toList
     println("The path of the chip is: " + path.mkString("[ ", ", ", " ]"))
     println("The winnings are: $" + slotPrize(path.last))
+    true
   }    
 
+  def dropMany(): Boolean = {
+    val num = numChips
+    val slot = selectSlot
+    val winnings = getMultipleWinnings(slot).take(num)
+    printf("Total winnings are $%.2f\n", sum(winnings))
+    printf("Average winnings are $%.2f\n", avg(winnings))
+    true
+  }
+
+  def quit(): Boolean = {
+    println("Thanks for playing!")
+    false
+  }
+
   def menu(option: Int): Boolean = {
-    option match {
-      case 1 =>
-        dropOne
-        true
-      case 2 =>
-        println("selected 2")
-        true
-      case 3 =>
-        println("selected 3")
-        false 
-      case _ =>
-        println("Sorry, that command is not recognized")
+    actionMap.get(option) match {
+      case Some(f) => f()
+      case None =>
+        println("Sorry, that command is not recognized.")
         true
     }
   }
-  
 
   def main(args: Array[String]) = {
     var opt = -1
