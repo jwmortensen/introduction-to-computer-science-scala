f2e2193ad4db86b29689f7cdbca58cb43133b902
Jacob Mortensen <jacob.w.mortensen@gmail.com>
2015-04-06 09:21:03 -0600

    Updated paint function

--- cars/CarInventory.scala
+++ cars/CarInventory.scala
@@ -29,6 +29,9 @@ object CarInventory {
     StdIn.readInt
   }
 
+  /** returns true. prints total number of cars in inventory and summary info
+    * for each vehicle
+    */
   def currentInventory(): Boolean = {
     val size = inventory.length
     if (size == 1) 
@@ -39,13 +42,19 @@ object CarInventory {
     return true
   }
 
+  /** returns true. prints current cash balance. **/
   def currentBalance(): Boolean = {
     printf("Current Balance: $%.2f\n", balance)
     return true
   }
 
+  /** returns true. if the price of the new car is less than balance, 
+    * the car is added to the inventory and the price is subtracted
+    * from balance
+    */
   def buyCar(): Boolean = {
-    println("Please enter information about the car:")
+    println("Vehicle Information:")
+    println("====================")
     println("Name:")
     val name = StdIn.readLine
     println("Color:")
@@ -59,15 +68,24 @@ object CarInventory {
       balance -= price
       inventory =  new Car(name, color, price) :: inventory
     }
-      
     return true
   }
 
   def sellCar(): Boolean = {
+    println("Vehicle name:")
+    val name = StdIn.readLine
+    // Figure out how to remove car from inventory and add 
+    // its sell price to balance.
     return true
   }
 
   def paintCar(): Boolean = {
+    println("Vehicle name:")
+    val name = StdIn.readLine
+    println("New color:")
+    val color = StdIn.readLine
+    val car = inventory.filter(_.name == name).head
+    car.paint(color)
     return true
   }
 
